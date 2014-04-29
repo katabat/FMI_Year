25e4b85358e3c55f1e57ba25b4e2a3c13934b3fa
katabat <heds_1@hotmail.com>
2014-04-29 12:17:15 +0100

    Add 2013 data extract

--- FMI_year.py
+++ FMI_year.py
@@ -8,3 +8,13 @@ import matplotlib.pyplot as plt
 from matplotlib import rc
 import matplotlib.dates as md
 import datetime
+import FMI_xml as fmi
+
+# 2013
+data13 = fmi.getXMLroot('ENO_2013.xml')
+tmp = fmi.getXMLtimes(data13)
+lat, lon, days = zip(*tmp)
+daysfmt = np.array([datetime.datetime.fromtimestamp(days[i]) for i in xrange(len(days))])
+
+data = fmi.getXMLdata(data13)
+rrday13, tday13, snow13, tmin13, tmax13 = zip(*data)
