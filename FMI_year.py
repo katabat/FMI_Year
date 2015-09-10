46101e91c774fe0c27e4933f889a1c7e0af4d029
Steptoe, Hamish <hamish.steptoe@metoffice.gov.uk>
2015-09-10 14:28:57 +0100

    Update FMI_year.py to use pandas DataFrame for data organisation

--- FMI_year.py
+++ FMI_year.py
@@ -9,191 +9,23 @@ from matplotlib import rc
 import matplotlib.dates as md
 import datetime
 import FMI_xml as fmi
+import glob
+import pandas as pd
 
+xmlfiles = glob.glob('*.xml')
 
-###################################
-# 2013
-data13 = fmi.getXMLroot('ENO_2013.xml')
-tmp = fmi.getXMLtimes(data13)
-lat, lon, days = zip(*tmp)
-daysfmt13 = np.array([datetime.datetime.fromtimestamp(days[i]) for i in xrange(len(days))])
-datefmt13 = [x.date() for x in daysfmt13]
+ENO = {}
 
+for f in xmlfiles:
+    d = fmi.getXMLroot(f)
+    tmp = fmi.getXMLtimes(d)
+    lat, lon, days = zip(*tmp)
+    data = fmi.getXMLdata(d)
+    rrday, tday, snow, tmin, tmax = zip(*data)
+    
+    daysfmt = np.array([datetime.datetime.fromtimestamp(days[i]) for i in xrange(len(days))])
+    ENO[f[4:8]] = pd.DataFrame({'rrday': rrday, 'tday':tday, 'snow':snow, 'tmin':tmin, 'tmax':tmax}, index= daysfmt)
 
-data = fmi.getXMLdata(data13)
-rrday13, tday13, snow13, tmin13, tmax13 = zip(*data)
-
-###################################
-# 2012
-data12 = fmi.getXMLroot('ENO_2012.xml')
-tmp = fmi.getXMLtimes(data12)
-
-lat, lon, days = zip(*tmp)
-daysfmt12 = np.array([datetime.datetime.fromtimestamp(days[i]) for i in xrange(len(days))])
-datefmt12 = [x.date() for x in daysfmt12]
-
-data = fmi.getXMLdata(data12)
-data = np.delete(data, (59), axis=0) # Delete leap day
-rrday12, tday12, snow12, tmin12, tmax12 = zip(*data)
-
-###################################
-# 2011
-data11 = fmi.getXMLroot('ENO_2011.xml')
-tmp = fmi.getXMLtimes(data11)
-
-lat, lon, days = zip(*tmp)
-daysfmt11 = np.array([datetime.datetime.fromtimestamp(days[i]) for i in xrange(len(days))])
-datefmt11 = [x.date() for x in daysfmt11]
-
-
-data = fmi.getXMLdata(data11)
-rrday11, tday11, snow11, tmin11, tmax11 = zip(*data)
-
-###################################
-# 2010
-data10 = fmi.getXMLroot('ENO_2010.xml')
-tmp = fmi.getXMLtimes(data10)
-
-lat, lon, days = zip(*tmp)
-daysfmt10 = np.array([datetime.datetime.fromtimestamp(days[i]) for i in xrange(len(days))])
-datefmt10 = [x.date() for x in daysfmt10]
-
-
-data = fmi.getXMLdata(data10)
-rrday10, tday10, snow10, tmin10, tmax10 = zip(*data)
-
-###################################
-# 2009
-data09 = fmi.getXMLroot('ENO_2009.xml')
-tmp = fmi.getXMLtimes(data09)
-
-lat, lon, days = zip(*tmp)
-daysfmt09 = np.array([datetime.datetime.fromtimestamp(days[i]) for i in xrange(len(days))])
-datefmt09 = [x.date() for x in daysfmt09]
-
-
-data = fmi.getXMLdata(data09)
-rrday09, tday09, snow09, tmin09, tmax09 = zip(*data)
-
-###################################
-# 2008
-data08 = fmi.getXMLroot('ENO_2008.xml')
-tmp = fmi.getXMLtimes(data08)
-
-lat, lon, days = zip(*tmp)
-daysfmt08 = np.array([datetime.datetime.fromtimestamp(days[i]) for i in xrange(len(days))])
-datefmt08 = [x.date() for x in daysfmt08]
-
-
-data = fmi.getXMLdata(data08)
-data = np.delete(data, (59), axis=0) # Delete leap day
-rrday08, tday08, snow08, tmin08, tmax08 = zip(*data)
-
-###################################
-# 2007
-data07 = fmi.getXMLroot('ENO_2007.xml')
-tmp = fmi.getXMLtimes(data07)
-
-lat, lon, days = zip(*tmp)
-daysfmt07 = np.array([datetime.datetime.fromtimestamp(days[i]) for i in xrange(len(days))])
-datefmt07 = [x.date() for x in daysfmt07]
-
-
-data = fmi.getXMLdata(data07)
-rrday07, tday07, snow07, tmin07, tmax07 = zip(*data)
-
-###################################
-# 2006
-data06 = fmi.getXMLroot('ENO_2006.xml')
-tmp = fmi.getXMLtimes(data06)
-
-lat, lon, days = zip(*tmp)
-daysfmt06 = np.array([datetime.datetime.fromtimestamp(days[i]) for i in xrange(len(days))])
-datefmt06 = [x.date() for x in daysfmt06]
-
-
-data = fmi.getXMLdata(data06)
-rrday06, tday06, snow06, tmin06, tmax06 = zip(*data)
-
-###################################
-# 2005
-data05 = fmi.getXMLroot('ENO_2005.xml')
-tmp = fmi.getXMLtimes(data05)
-
-lat, lon, days = zip(*tmp)
-daysfmt05 = np.array([datetime.datetime.fromtimestamp(days[i]) for i in xrange(len(days))])
-datefmt05 = [x.date() for x in daysfmt05]
-
-
-data = fmi.getXMLdata(data05)
-rrday05, tday05, snow05, tmin05, tmax05 = zip(*data)
-
-###################################
-# 2004
-data04 = fmi.getXMLroot('ENO_2004.xml')
-tmp = fmi.getXMLtimes(data04)
-
-lat, lon, days = zip(*tmp)
-daysfmt04 = np.array([datetime.datetime.fromtimestamp(days[i]) for i in xrange(len(days))])
-datefmt04 = [x.date() for x in daysfmt04]
-
-
-data = fmi.getXMLdata(data04)
-data = np.delete(data, (59), axis=0) # Delete leap day
-rrday04, tday04, snow04, tmin04, tmax04 = zip(*data)
-
-###################################
-# 2003
-data03 = fmi.getXMLroot('ENO_2003.xml')
-tmp = fmi.getXMLtimes(data03)
-
-lat, lon, days = zip(*tmp)
-daysfmt03 = np.array([datetime.datetime.fromtimestamp(days[i]) for i in xrange(len(days))])
-datefmt03 = [x.date() for x in daysfmt03]
-
-
-data = fmi.getXMLdata(data03)
-rrday03, tday03, snow03, tmin03, tmax03 = zip(*data)
-
-###################################
-# 2002
-data02 = fmi.getXMLroot('ENO_2002.xml')
-tmp = fmi.getXMLtimes(data02)
-
-lat, lon, days = zip(*tmp)
-daysfmt02 = np.array([datetime.datetime.fromtimestamp(days[i]) for i in xrange(len(days))])
-datefmt02 = [x.date() for x in daysfmt02]
-
-
-data = fmi.getXMLdata(data02)
-rrday02, tday02, snow02, tmin02, tmax02 = zip(*data)
-
-###################################
-# 2001
-data01 = fmi.getXMLroot('ENO_2001.xml')
-tmp = fmi.getXMLtimes(data01)
-
-lat, lon, days = zip(*tmp)
-daysfmt01 = np.array([datetime.datetime.fromtimestamp(days[i]) for i in xrange(len(days))])
-datefmt01 = [x.date() for x in daysfmt01]
-
-
-data = fmi.getXMLdata(data01)
-rrday01, tday01, snow01, tmin01, tmax01 = zip(*data)
-
-###################################
-# 2000
-# data00 = fmi.getXMLroot('ENO_2000.xml')
-# tmp = fmi.getXMLtimes(data00)
-#
-# lat, lon, days = zip(*tmp)
-# daysfmt00 = np.array([datetime.datetime.fromtimestamp(days[i]) for i in xrange(len(days))])
-# datefmt00 = [x.date() for x in daysfmt00]
-#
-#
-# data = fmi.getXMLdata(data00)
-# data = np.delete(data, (59), axis=0) # Delete leap day
-# rrday00, tday00, snow00, tmin00, tmax00 = zip(*data)
 
 tdayall = zip(tday13, tday12, tday11, tday10, tday09, tday08, tday06, tday05,
               tday04, tday03, tday02, tday01) #2007 data incomplete, ignored
@@ -302,7 +134,7 @@ ax1.set_ylabel('Temperature ($^{\circ}$C)')
 ax1.yaxis.set_label_coords(-0.05, 0.55)
 ax2.set_ylabel('Snow Cover (cm)')
 ax2.yaxis.set_label_coords(1.05, 0.1)
-fig.savefig('test.eps', papertype = 'a4', orientation = 'portrait', format='eps', dpi=1000)
+#fig.savefig('test.eps', papertype = 'a4', orientation = 'portrait', format='eps', dpi=1000)
 # ax1.grid(True)
 # ax2.grid(True)
 plt.show()
